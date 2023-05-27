befb381f9cad332d4a0029bbf78dc46223812123
canhamzacode <abdulmuizzayo6@gmail.com>
2023-05-27 11:14:58 -0700

    added all files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is a function to print with puts
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: this is a function to print with printf
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+}
