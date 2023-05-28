40328aa46b391d9f7a1822d6ce7260bd1b8a0a3c
canhamzacode <abdulmuizzayo6@gmail.com>
2023-05-28 11:05:47 +0200

    updated the readme also created the first file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starting point
+ * Description: a function that generates a random number and reurn result based on condition
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n > 0)
+	{
+		printf("%d is positive",n);
+	}
+	else
+	{
+		printf("%d is negative",n);
+	}
+	return (0);
+}
