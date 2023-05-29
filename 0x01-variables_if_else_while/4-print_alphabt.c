64c9086b1691a8419558d14e9e6063482141b769
canhamzacode <abdulmuizzayo6@gmail.com>
2023-05-29 04:25:45 -0700

    mved file it it normal position

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function initialization
+ * Description: print from a - z excluding q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		if (alp == 'q' || alp == 'e')
+		{
+			alp++;
+		}
+		else
+		{
+			putchar(alp);
+			alp++;
+		}
+	}
+	putchar('\n');
+
+}
