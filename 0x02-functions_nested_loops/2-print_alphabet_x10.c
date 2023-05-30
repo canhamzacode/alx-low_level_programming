b8ed3b4ac0db61018d5e3e998b6afb10b2eade63
canhamzacode <abdulmuizzayo6@gmail.com>
2023-05-30 14:40:15 -0700

    added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,6 +11,7 @@ void print_alphabet_x10(void)
 	while (i < 10)
 	{
 		char alp = 'a';
+
 		while (alp <= 'z')
 		{
 			_putchar(alp);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 void print_alphabet_x10(void);
 int _putchar(char c);
+int _islower(int c);
 
 #endif
