f5e28e59c1eb5c168f493d6c5dc239f3a7f36761
canhamzacode <abdulmuizzayo@gmail.com>
2023-06-07 05:41:13 -0700

    added the 0-puts_recussion file it compiled perfectly

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string then a new line
+ * @s: first param
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <stdio.h>
+void _puts_recursion(char *s);
+
+
+#endif
