ba5358881a6a4074b0e6aed4d9a9140c30476443
canhamzacode <abdulmuizzayo6@gmail.com>
2023-06-09 12:41:00 +0200

    learnt static library also the differences between static and dynamic library. How create my own wasnt left behind also

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - is it a digit
+ * @c: param
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c > 0 && c < 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
