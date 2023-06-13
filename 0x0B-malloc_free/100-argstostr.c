7440e03ad602eb5a8ebd6c4ef3cafb3c1dabe3e2
canhamzacode <abdulmuizzayo@gmail.com>
2023-06-13 16:36:33 -0700

    fixing error

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,15 +13,15 @@ char *argstostr(int ac, char **av)
 	char *result;
 
 	if (ac == 0 || av == NULL)
-		return NULL;
+		return (NULL);
 
 	total_length = 0;
 	for (i = 0; i < ac; i++)
 		total_length += strlen(av[i]) + 1; /* +1 for the newline character */
 
-	result = (char *)malloc((total_length + 1) * sizeof(char)); /* +1 for the null terminator */
+	result = (char *)malloc((total_length + 1) * sizeof(char)); /* +1 for the*/
 	if (result == NULL)
-		return NULL;
+		return (NULL);
 
 	index = 0;
 	for (i = 0; i < ac; i++)
@@ -34,5 +34,5 @@ char *argstostr(int ac, char **av)
 
 	result[total_length] = '\0';
 
-	return result;
+	return (result);
 }
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,87 +1,77 @@
+#include <stdlib.h>
 #include "main.h"
 
-int count_words(char *str);
-char **strtow(char *str);
-
 /**
- * count_words - Counts the number of words in a string
- * @str: The input string
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- * Return: The number of words in the string
+ * Return: number of words
  */
-int count_words(char *str)
+int count_word(char *s)
 {
-        int count = 0;
-        int i = 0;
+	int flag, c, w;
 
-        while (str[i] != '\0')
-        {
-                if (str[i] != ' ')
-                {
-                        count++;
-                        while (str[i] != ' ' && str[i] != '\0')
-                                i++;
-                }
-                else
-                        i++;
-        }
+	flag = 0;
+	w = 0;
 
-        return count;
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
 }
-
 /**
- * strtow - Splits a string into words
- * @str: The input string
+ * **strtow - a funktion splits a string into words
+ * @str: string to split
  *
- * Return: A pointer to an array of strings (words), or NULL if it fails
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-        int i, j, k, word_length, word_count;
-        char **words;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-        if (str == NULL || *str == '\0')
-                return NULL;
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
 
-        word_count = count_words(str);
-        words = (char **)malloc((word_count + 1) * sizeof(char *));
-        if (words == NULL)
-                return NULL;
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
 
-        i = 0;
-        j = 0;
-        k = 0;
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
 
-        while (str[i] != '\0')
-        {
-                if (str[i] != ' ')
-                {
-                        word_length = 0;
+	matrix[k] = NULL;
 
-                        while (str[i] != ' ' && str[i] != '\0')
-                        {
-                                word_length++;
-                                i++;
-                        }
-
-                        words[j] = (char *)malloc((word_length + 1) * sizeof(char));
-                        if (words[j] == NULL)
-                        {
-                                for (k = 0; k < j; k++)
-                                        free(words[k]);
-                                free(words);
-                                return NULL;
-                        }
-
-                        strncpy(words[j], &str[i - word_length], word_length);
-                        words[j][word_length] = '\0';
-                        j++;
-                }
-                else
-                        i++;
-        }
-
-        words[j] = NULL;
-
-        return words;
+	return (matrix);
 }
