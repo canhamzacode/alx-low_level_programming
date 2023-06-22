593b79d88b22f7c6cb7cf84a6f8d94ca3f0db21a
canhamzacode <abdulmuizzayo6@gmail.com>
2023-06-22 22:55:53 +0200

    first task complted all checks also complted

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * a function to print a name
+ * @name: name to be printed
+ * @f: function pointer
+ * Return: nothing
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
